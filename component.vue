309cac747ef9d1d0ab837ddb78b76181ba5027a8
Ilya Klyukovskiy <48031577+klyuk88@users.noreply.github.com>
2022-01-08 11:39:43 +0300

    1
    1

---
+++ component.vue
@@ -0,0 +1,27 @@
+
+<template>
+<a href="#" @click.prevent="toAnchor">To Anchor</a>
+
+<div id="anchor">
+With Anchor section
+</div>
+</template>
+
+<script>
+import {gsap} from 'gsap';
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+setup() {
+gsap.registerPlugin(ScrollToPlugin);
+const toAnchor = () => {
+      gsap.to(window, {
+        duration: 1,
+        scrollTo: props.anchor,
+        ease: "power2",
+      });
+    };
+    
+    return {
+toAnchor
+}
+</script>
